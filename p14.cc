20042df5a87c7cd7185fb0114fab149afd99494f
Katsuaki Oshio <katsuaki.oshio@gmail.com>
2019-05-05 21:47:45 +0900

    commit p14

---
+++ p14.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+bool p14(std::string isbn) {
+    if (isbn.length() != 10) {
+        return false;
+    }
+
+    int check_digit = isbn[9] - '0';
+    isbn.erase(9);
+
+    int w = 10;
+    int sum = 0;
+    for (auto& c : isbn) {
+        sum += w * (c - '0');
+        --w;
+    }
+
+    return check_digit == (11 - (sum % 11));
+}
+
+int main() {
+    std::cout << p14("4764902222") << std::endl;
+    std::cout << p14("3764902222") << std::endl;
+
+    return 0;
+}
