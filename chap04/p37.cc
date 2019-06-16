4b0e331d14419e286c56d921259bd970d8df46b2
Katsuaki Oshio <katsuaki.oshio@gmail.com>
2019-06-16 17:44:20 +0900

    add p37

--- chap04/p37.cc
+++ chap04/p37.cc
@@ -1,3 +1,31 @@
+#include <experimental/filesystem>
 #include <iostream>
+#include <regex>
 
-int main() { return 0; }
+namespace fs = std::experimental::filesystem;
+
+std::vector<fs::directory_entry> find_file(fs::path& dir, std::string pattern) {
+  std::vector<fs::directory_entry> result;
+  std::regex rx(pattern.c_str());
+  auto iterator = fs::recursive_directory_iterator(dir);
+
+  for (auto x : iterator) {
+    // std::cout << x.path() << std::endl;
+    if (fs::is_regular_file(x) &&
+        std::regex_match(x.path().filename().string(), rx)) {
+      result.push_back(x);
+    }
+  }
+
+  return result;
+}
+
+int main(int argc, char* argv[]) {
+  fs::path path = R"(../)";
+  std::string pattern = R"(.+cc)";
+  auto result = find_file(path, pattern);
+
+  for (auto& x : result) {
+    std::cout << x.path() << std::endl;
+  }
+}
