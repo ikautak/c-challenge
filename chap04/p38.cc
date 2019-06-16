6735aa751eb7a6e0b8a97ca6dc9a2958e3988a94
Katsuaki Oshio <katsuaki.oshio@gmail.com>
2019-06-16 17:59:53 +0900

    add p38

--- chap04/p38.cc
+++ chap04/p38.cc
@@ -1,3 +1,64 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <experimental/filesystem>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
-int main() { return 0; }
+namespace fs = std::experimental::filesystem;
+
+class Log {
+  fs::path path_;
+  std::ofstream file_;
+
+ public:
+  Log() {
+    std::ostringstream oss;
+
+    pid_t pid = getpid();
+    oss << pid;
+    oss << "_log";
+    std::cout << oss.str() << std::endl;
+
+    path_ = fs::temp_directory_path() / (oss.str());
+    file_.open(path_.c_str(), std::ios::out | std::ios::trunc);
+  }
+
+  ~Log() noexcept {
+    try {
+      if (file_.is_open()) {
+        file_.close();
+      }
+
+      if (!path_.empty()) {
+        fs::remove(path_);
+      }
+    } catch (...) {
+    }
+  }
+
+  void persist(fs::path const& path) {
+    file_.close();
+    fs::rename(path_, path);
+    path_.clear();
+  }
+
+  Log& operator<<(const std::string& message) {
+    file_ << message.c_str() << '\n';
+    return *this;
+  }
+};
+
+int main() {
+  Log log;
+
+  try {
+    log << "this is a line"
+        << "and this is another one";
+    throw std::runtime_error("error");
+  } catch (...) {
+    log.persist(R"(lastlog.txt)");
+  }
+
+  return 0;
+}
