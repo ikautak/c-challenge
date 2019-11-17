7ab6d8c92a832a1669e2e7a2c35acf35e66ce70d
Katsuaki Oshio <oshio@fixstars.com>
2019-11-17 17:46:36 +0900

    p95

--- chap12/p95.cc
+++ chap12/p95.cc
@@ -1,5 +1,32 @@
 #include <iostream>
 
+#define ASIO_STANDALONE
+#include "asio.hpp"
+
+std::vector<std::string> get_ip_address(std::string const& hostname) {
+  std::vector<std::string> ips;
+
+  try {
+    asio::io_context context;
+    asio::ip::tcp::resolver resolver(context);
+    auto endpoints =
+        resolver.resolve(asio::ip::tcp::v4(), hostname.c_str(), "");
+
+    for (auto const& e : endpoints) {
+      ips.push_back(e.endpoint().address().to_string());
+    }
+  } catch (std::exception const& e) {
+    std::cerr << "exception: " << e.what() << std::endl;
+  }
+
+  return ips;
+}
+
 int main() {
-    return 0;
+  auto ips = get_ip_address("packtpub.com");
+  for (auto const& ip : ips) {
+    std::cout << ip << std::endl;
+  }
+
+  return 0;
 }
